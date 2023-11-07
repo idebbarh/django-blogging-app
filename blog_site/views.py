1cc2ac35e8b00c82a24614cf49764c04eeda84b4
ismail debbarh <usamdh07@gmail.com>
2023-11-07 12:59:19 +0100

    feat: add landing section with random generated image

--- blog_site/views.py
+++ blog_site/views.py
@@ -1,9 +1,16 @@
+import os
+import random
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from blog_site.settings import BASE_DIR
 
+images_dir = BASE_DIR / "static/assets/images/"
+
+images_paths = [image for image in os.listdir(images_dir) if os.path.isfile(os.path.join(images_dir,image))]
 
 def index(_):
-    file_content = render_to_string("blog_site/home.html")
+    random_image_index = random.randrange(0,len(images_paths)-1)
+    file_content = render_to_string("blog_site/home.html",{"landing_image":images_paths[random_image_index]})
     return HttpResponse(file_content)
 
 
